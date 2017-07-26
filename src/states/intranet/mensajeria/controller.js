9563d614e53401e2c5e62db82f94022d7f5eafe6
Jesús Villanueva <jvillanueva@hyosystem.com>
2017-07-26 18:31:41 -0500

    Ui-view transition Optimized

--- src/states/intranet/mensajeria/controller.js
+++ src/states/intranet/mensajeria/controller.js
@@ -2,8 +2,24 @@
   return Math.floor(Math.random() * (max - min + 1) + min)
 } */
 /* @ngInject */
-export default function app () {
+export default function app ($timeout) {
   // $scope.$parent.$sie
   const $sie = this
-  console.log($sie)
+  const size = 50000// 500000 muere
+  $sie.generarArreglo = function () {
+    console.time('arreglo')
+    const arreglo = []
+    for (let i = 0; i < size; i++) {
+      arreglo.push({id: i, value: `Item${i}`})
+    }
+    $sie.data = arreglo
+    $timeout(() => {
+      console.timeEnd('arreglo')
+    })
+  }
+
+  $sie.$onInit = () => {
+    $sie.data = []
+    console.log('inicio')
+  }
 }
